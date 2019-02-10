a52a06b86821d3dd47f8fc42cbdcd905ea30dacd
inigma13 <46365188+inigma13@users.noreply.github.com>
2019-02-10 14:19:15 +1300

    Initial commit

---
+++ app/AppKernel.php
@@ -0,0 +1,19 @@
+<?php
+// app/AppKernel.php
+
+// ...
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        $bundles = array(
+            // ...
+
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+        );
+
+        // ...
+    }
+
+    // ...
+}
---
+++ model/Login.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * @ORM\Entity
+ * @orm\table(name="login")
+ */
+
+class Login
+{
+    /**
+     * @orm:Column(type="string", nullable=false)
+     * @assert:NotBlank
+     */
+    var $emailAddress;
+    var $password;
+    var $online;
+
+    /**
+     * Login constructor.
+     * @param $emailAddress
+     * @param $password
+     * @param $online
+     */
+    public function __construct($emailAddress, $password, $online)
+    {
+        $this->emailAddress = $emailAddress;
+        $this->password = $password;
+        $this->online = $online;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmailAddress()
+    {
+        return $this->emailAddress;
+    }
+
+    /**
+     * @param mixed $emailAddress
+     */
+    public function setEmailAddress($emailAddress)
+    {
+        $this->emailAddress = $emailAddress;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOnline()
+    {
+        return $this->online;
+    }
+
+    /**
+     * @param mixed $online
+     */
+    public function setOnline($online)
+    {
+        $this->online = $online;
+    }
+
+
+}
